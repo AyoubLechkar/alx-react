a0776e0feb3d5e7d499f4c605a725327ed52f2b8
AyoubLechkar <lechayoub1@gmail.com>
2024-02-24 01:13:18 +0100

    task 3

--- 0x01-react_intro/task_3/dashboard/src/App.js
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -1,34 +1,29 @@
-import React, { Component } from 'react'
-import logo from './logo.jpg'
-import { getFullYear, getFooterCopy } from './utils'
+import logo from './logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
 
-export default class App extends Component {
-	render() {
-		return (
-			<div className="App">
-	  			<header className="App-header">
-		  			<img src={logo} className="App-logo" alt="logo" />
-		  			<h1>School dashboard</h1>
-	  			</header>
-	 			<body className="App-body">
-		 		 	<p>Login to access the full dashboard</p>
-					<label htmlFor="email" onClick={() => {
-						// select corresponding input
-						document.getElementById('password').focus();
-					}}>Email</label>
-					<input type="email" id="email" />
-					<label htmlFor="password" onClick={() => {
-						// select corresponding input
-						document.getElementById('password').focus();
-					}}>Password</label>
-					<input type="password" id="password" />
-					<button>OK</button>
-	  			</body>
-				<footer className="App-footer">
-					<p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-				</footer>
-			</div>
-		)
-	}
+function App() {
+  return (
+    <>
+      <header className='App-header'>
+        <img src={logo} alt='logo' />
+        <h1>School dashboard</h1>
+      </header>
+      <main role='main' className='App-body'>
+        <p>Login to access the full dashboard</p>
+        <label htmlFor='email'>Email</label>
+        <input type='email' name='email' id='email' />
+        <label htmlFor='password'>Password</label>
+        <input type='password' name='password' id='password' />
+        <button type='button'>OK</button>
+      </main>
+      <footer className='App-footer'>
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </footer>
+    </>
+  );
 }
 
+export default App;
